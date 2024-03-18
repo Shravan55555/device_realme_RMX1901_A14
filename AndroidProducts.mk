55248aa3e040aa08ab059e6c43ed36d57eff330d
Shravankumar <shravankumarsayz@gmail.com>
2024-03-18 10:51:22 +0530

    RMX1901: Pixelstar

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_RMX1901.mk
+    $(LOCAL_DIR)/pixelstar_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_RMX1901-userdebug \
-    blaze_RMX1901-eng
+    pixelstar_RMX1901-userdebug \
+    poxelstar_RMX1901-eng
