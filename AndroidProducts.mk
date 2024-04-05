26a35c6bb19a3a8b4cb8077782c3adc9297f3d70
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2024-04-05 11:54:30 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/sigma_RMX1901.mk
+    $(LOCAL_DIR)/aospa_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    sigma_RMX1901-userdebug \
-    sigma_RMX1901-eng
+    aospa_RMX1901-userdebug \
+    aospa_RMX1901-eng
